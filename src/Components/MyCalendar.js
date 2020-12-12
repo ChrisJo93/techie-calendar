73d186e04abd25cf2f66420bb78e7a53ccd14c24
Chris Johnson <Johnny.C.Alexander@gmail.com>
2020-12-12 10:08:35 -0600

    imported moment and big calendar

---
+++ src/Components/MyCalendar.js
@@ -0,0 +1,18 @@
+// import BigCalendar from 'react-big-calendar';
+// import React, { Component } from 'react';
+// import moment from 'moment';
+
+// const localizer = BigCalendar.momentLocalizer(moment);
+
+// const MyCalendar = (props) => (
+//   <div>
+//     <BigCalendar
+//       localizer={localizer}
+//       events={myEventsList}
+//       startAccessor="start"
+//       endAccessor="end"
+//     />
+//   </div>
+// );
+
+// export default MyCalendar;
