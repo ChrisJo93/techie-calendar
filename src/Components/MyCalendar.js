7388fcc504e9ddf0951fc9c1407a5a4cec91200e
Chris Johnson <Johnny.C.Alexander@gmail.com>
2020-12-25 17:48:19 -0600

    added customized year view to calendar. Bug fixing. Calendar needs an edit feature for dates

--- src/Components/MyCalendar.js
+++ src/Components/MyCalendar.js
@@ -3,27 +3,41 @@ import React, { Component } from 'react';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import events from './events';
+import Year from './Year';
 
 const localizer = momentLocalizer(moment);
+localizer.formats.yearHeaderFormat = 'YYYY';
 
 class MyCalendar extends Component {
   state = {
     events: events,
     view: 'week',
-    date: new Date(2015, 3, 12),
+    date: new Date(2015, 0, 1),
   };
 
   render() {
     return (
       <div style={{ height: '700pt' }}>
         <Calendar
+          selectable
           events={this.state.events}
-          // onClick={console.log(this.state.events[0].title)}
+          defaultView="week"
+          date={this.state.date}
+          onNavigate={(date) => {
+            this.setState({ date: date });
+          }}
+          onClick={console.log(this.state.date)}
           startAccessor="start"
           endAccessor="end"
-          date={this.state.date}
-          onNavigate={(date) => this.setState({ date })}
           localizer={localizer}
+          views={{
+            day: true,
+            week: true,
+            month: true,
+            year: Year,
+          }}
+          messages={{ year: 'Year' }}
+          toolbar={true}
         />
       </div>
     );
